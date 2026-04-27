a29a7c8d6ece2251b6675dffcf2ea0bb503a38b3
Nico Dann <nicodann@gmail.com>
2026-04-27 13:56:45 -0400

    Create prompt form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
+type FormValues = {
+  prompt: string;
+};
+
 function App() {
-  return <section id="center"></section>;
+  const [formValues, setFormValues] = useState<FormValues>({ prompt: "" });
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("form submitted", formValues);
+  };
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label>Describe a scene or feeling.</label>
+        <input
+          type="text"
+          value={formValues.prompt}
+          placeholder="A misty lake full of poison fishies"
+          onChange={(e) =>
+            setFormValues((prev) => {
+              return { ...prev, prompt: e.target.value };
+            })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
