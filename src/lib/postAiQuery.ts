e77774f6885ceb0899737f9c737ee28434013b70
Nico Dann <nicodann@gmail.com>
2026-05-12 14:11:22 -0400

    Amend api route name.

--- src/lib/postAiQuery.ts
+++ src/lib/postAiQuery.ts
@@ -2,7 +2,7 @@ export async function postAiQuery({ query }: { query: string }) {
   const url = import.meta.env.VITE_API_URL;
 
   try {
-    const response = await fetch(`${url}/api-query`, {
+    const response = await fetch(`${url}/ai-query`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ input: query }),
